100bf7689cc31a8d65a0e4782322201bcb5e912b
logiclabz <logiclabz@gmail.com>
2017-08-08 21:52:05 +0530

    add workbox sw

---
+++ sw.js
@@ -0,0 +1,44 @@
+importScripts('workbox-sw.prod.v1.1.0.js');
+
+/**
+ * DO NOT EDIT THE FILE MANIFEST ENTRY
+ *
+ * The method precache() does the following:
+ * 1. Cache URLs in the manifest to a local cache.
+ * 2. When a network request is made for any of these URLs the response
+ *    will ALWAYS comes from the cache, NEVER the network.
+ * 3. When the service worker changes ONLY assets with a revision change are
+ *    updated, old cache entries are left as is.
+ *
+ * By changing the file manifest manually, your users may end up not receiving
+ * new versions of files because the revision hasn't changed.
+ *
+ * Please use workbox-build or some other tool / approach to generate the file
+ * manifest which accounts for changes to local files and update the revision
+ * accordingly.
+ */
+const fileManifest = [
+  {
+    "url": "css/main.css",
+    "revision": "8170eaac3981578a0404bff3034110ad"
+  },
+  {
+    "url": "css/normalize.css",
+    "revision": "1e9c0f3b13da66012ea079bc1509fef2"
+  },
+  {
+    "url": "favicon.ico",
+    "revision": "01a64ba48ad9146a08fac0a6946b7521"
+  },
+  {
+    "url": "img/logo.png",
+    "revision": "fe239db7c0e5347aac87fa6ac8403c10"
+  },
+  {
+    "url": "img/logo.svg",
+    "revision": "777a89d70b94343eafc8ec602c05371f"
+  }
+];
+
+const workboxSW = new self.WorkboxSW();
+workboxSW.precache(fileManifest);
---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "**/*.{css,ico,png,svg}"
+  ],
+  "swDest": "./sw.js",
+  "globIgnores": [
+    "workbox-cli-config.js"
+  ]
+};
